939c43492db4acf40f0de4fa45e0e69615c10213
bbkmg256 <tecded@proton.me>
2026-08-16 03:14:36 -0300

    Agregado script de preparacion para despliegue, no terminado

---
+++ tools/prep.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script para preparar el entorno de [V-T] (PENDIENTE, NO EJECUTAR!!!!) #
+
+
+# Esto tiene que comprobar primero si existen o no los directorios correspondientes
+# antes de crear sin mas, así se evita sobre escribir contenido que ya pueda existir.
+
+# Crea directorios de contenido
+mkdir ../media # Almacena contenido multimedia subido por los usuarios
+mkdir ../statics/banners # Almacena los banneres que apareceran al principio de cada tablón
